aa21e841b5db9814a6951ea73cd9de5bb60028c2
Matheus Schreiber <mt.schreiber01@gmail.com>
2025-03-19 01:19:05 -0300

    :fire: add env variables

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -9,7 +9,7 @@ import { toast } from 'sonner';
 
 class ServiceFactoryContract {
 
-    localProviderEnabled: boolean = true;
+    localProviderEnabled: boolean = import.meta.env.VITE_LOCAL_PROVIDER_ENABLED == 'true';
     contractAddressFactory: string = localStorage.getItem("contractAddress") || "";
     serviceFactoryABI: any = ServiceFactoryArtifact.abi
     serviceABI: any = ServiceArtifact.abi
