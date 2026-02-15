2cd9b0e4142a2e9040377d28abc434c06dd28d51
Matheus Schreiber <matheusmeier.sch2341@gmail.com>
2026-02-15 21:50:06 +0100

    :memo: add sepolia disclaimer

--- src/components/topper.tsx
+++ src/components/topper.tsx
@@ -1,4 +1,4 @@
-import { Check, Loader2, Plus, Sparkle, X } from "lucide-react";
+import { Check, CheckCircle2Icon, Loader2, Plus, Sparkle, X } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
@@ -55,45 +55,61 @@ export function Topper() {
     }, [status])
 
     return (
-        <header className="flex flex-col items-center justify-center mb-16">
-
-            {window.location.pathname != '/' && (
-                <img src="/logo.png" alt="Logo" className="w-[200px] cursor-pointer mb-16" onClick={() => navigate("/")} />
-            )}
-
-            <div className="flex lg:flex-row flex-col items-center gap-5">
-                {
-                    account.isConnected ? (
-                        <Alert className={loading ? "text-zinc-400" : "text-green-400"}>
-                            {loading ? <Loader2 className="animate-spin" /> : <Check className="h-4 w-4" />}
-                            <AlertTitle>
-                                <div className="flex items-center justify-between">
-                                    <p>{loading ? "Loading..." : "Wallet connected"}</p>
-                                    <p className="text-red-400"><X onClick={()=>disconnectWallet()} className="cursor-pointer"/></p>
-                                </div>
-                            </AlertTitle>
-                            <AlertDescription>
-                                Wallet successfully connected. <br /> <p className="text-zinc-700">{walletAddress}</p>
-                            </AlertDescription>
-                        </Alert>
-                    ) : (
-                        <Alert className={loading ? "text-zinc-400" : "text-red-400"}>
-                            {loading ? <Loader2 className="animate-spin" /> : <Sparkle className="h-4 w-4" />}
-                            <AlertTitle>
-                                {loading ? "Loading..." : "Wallet not connected"}
-                            </AlertTitle>
-                            <AlertDescription>
-                                To use the dApp, connect to you Metamask wallet.
-                                <Button variant="secondary" onClick={() => connectWallet()} disabled={loading} className="mt-4 cursor-pointer">
-                                    {loading ? <><Loader2 className="animate-spin" />Connecting...</> : <><Plus /> Connect Wallet</>}
-                                </Button>
-                            </AlertDescription>
-                        </Alert>
-                    )
-                }
+        <div className="flex justify-center w-full">
+            <div className="absolute z-10 w-full">
+                <Alert className="w-128 ml-4">
+                    <CheckCircle2Icon />
+                    <AlertTitle>Attention</AlertTitle>
+                    <AlertDescription className="leading-none">
+                        <p>
+                            This project uses <b>Sepolia ETH</b>, which is for testing 
+                            purposes only. <b className="text-red-400">DO NOT USE REAL ETH</b> or 
+                            any valuable assets on this network, 
+                            as they may be lost.
+                        </p>
+                    </AlertDescription>
+                </Alert>
             </div>
+            <header className="flex flex-col items-center justify-center mb-16">
 
-            <ContractEvents />
-        </header >
+                {window.location.pathname != '/' && (
+                    <img src="/logo.png" alt="Logo" className="w-[200px] cursor-pointer mb-16" onClick={() => navigate("/")} />
+                )}
+
+                <div className="flex lg:flex-row flex-col items-center gap-5">
+                    {
+                        account.isConnected ? (
+                            <Alert className={loading ? "text-zinc-400" : "text-green-400"}>
+                                {loading ? <Loader2 className="animate-spin" /> : <Check className="h-4 w-4" />}
+                                <AlertTitle>
+                                    <div className="flex items-center justify-between">
+                                        <p>{loading ? "Loading..." : "Wallet connected"}</p>
+                                        <p className="text-red-400"><X onClick={()=>disconnectWallet()} className="cursor-pointer"/></p>
+                                    </div>
+                                </AlertTitle>
+                                <AlertDescription>
+                                    Wallet successfully connected. <br /> <p className="text-zinc-700">{walletAddress}</p>
+                                </AlertDescription>
+                            </Alert>
+                        ) : (
+                            <Alert className={loading ? "text-zinc-400" : "text-red-400"}>
+                                {loading ? <Loader2 className="animate-spin" /> : <Sparkle className="h-4 w-4" />}
+                                <AlertTitle>
+                                    {loading ? "Loading..." : "Wallet not connected"}
+                                </AlertTitle>
+                                <AlertDescription>
+                                    To use the dApp, connect to you Metamask wallet.
+                                    <Button variant="secondary" onClick={() => connectWallet()} disabled={loading} className="mt-4 cursor-pointer">
+                                        {loading ? <><Loader2 className="animate-spin" />Connecting...</> : <><Plus /> Connect Wallet</>}
+                                    </Button>
+                                </AlertDescription>
+                            </Alert>
+                        )
+                    }
+                </div>
+
+                <ContractEvents />
+            </header >
+        </div>
     )
 }
